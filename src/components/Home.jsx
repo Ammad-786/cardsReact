90e20fa7227403aa8be6a65e1dcc372e74f8b9a4
Ammad-786 <ammadahmed63@protonmail.com>
2022-12-28 18:26:47 +0500

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+const Home = () => {
+  return (
+    <div>
+       <div className=" bg-[#333]  md:h-screen mx-auto p-20 flex flex-col items-center justify-center">
+            <h1 className="text-3xl font-bold text-center text-white">Welcome to the Salon</h1>
+            <p className="text-xl text-center text-[#b3b3b3] mb-8">We offer a wide range of beauty and hair services</p>
+            <div className="grid sm:grid-cols-2 md:grid-cols-3 justify-center gap-8 ">
+              <div className="card-wrapper">
+                <div className="card1 rounded-lg shadow-lg p-6 w-[280px] h-[280px] text-center">
+                  <div className='bg-white rounded-lg h-[100%] opacity-80 w-[100%] p-10'>
+                    <h2 className="text-lg font-bold text-gray-800 mb-4">Haircuts</h2>
+                    <p className="text-black mb-4">Get a fresh new haircut at our salon</p>
+                    <button className="bg-black text-white font-bold py-2 px-4 rounded-full">Book now</button>
+                  </div>
+                </div>
+              </div>
+
+              <div className="card-wrapper">
+                <div className="card1 rounded-lg shadow-lg p-6 w-[280px] h-[280px] text-center">
+                  <div className='bg-white rounded-lg h-[100%] opacity-80 w-[100%] p-10'>
+                    <h2 className="text-lg font-bold text-gray-800 mb-4">Haircuts</h2>
+                    <p className="text-black mb-4">Get a fresh new haircut at our salon</p>
+                    <button className="bg-black text-white font-bold py-2 px-4 rounded-full">Book now</button>
+                  </div>
+                </div>
+              </div>
+
+              <div className="card-wrapper">
+                <div className="card1 rounded-lg shadow-lg p-6 w-[280px] h-[280px] text-center">
+                  <div className='bg-white rounded-lg h-[100%] opacity-80 w-[100%] p-10'>
+                    <h2 className="text-lg font-bold text-gray-800 mb-4">Haircuts</h2>
+                    <p className="text-black mb-4">Get a fresh new haircut at our salon</p>
+                    <button className="bg-black text-white font-bold py-2 px-4 rounded-full">Book now</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div> 
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "../index.css"
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+
+const Navbar = () => {
+
+  return (
+    <div>
+        <nav  className="bg-black p-4 md:p-6 flex md:flex-col items-center justify-between border-b-2 border-white  md:justify-center">
+            <div className=" flex  text-2xl text-white justify-center">NEEDLE CUTS</div>
+            <div className="md:hidden">
+                <button className="text-gray-500 hover:text-white focus:text-white focus:outline-none">
+                <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                    <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+                </svg>
+                </button>
+            </div>
+            <div  className=" hidden md:flex justify-center">
+                <a href="/" className="text-white font-bold px-4 py-2 rounded-lg hover:bg-gray-700">Home</a>
+                <a href="/" className="text-white font-bold px-4 py-2 rounded-lg hover:bg-gray-700">About</a>
+                <a href="/" className="text-white font-bold px-4 py-2 rounded-lg hover:bg-gray-700">Services</a>
+                <a href="/" className="text-white font-bold px-4 py-2 rounded-lg hover:bg-gray-700">Contact</a>
+            </div>
+            <div className="social-icons hidden md:flex justify-center">
+                <a href="/"><FaFacebookF /></a>
+                <a href="/"><FaTwitter /></a>
+                <a href="/"><FaInstagram /></a>
+            </div>
+        </nav>
+            
+    </div>
+  )
+}
+
+export default Navbar
